93f5690ece2529e10cc0169fa9fe9d7ff64d6b3c
ragnar17 <edwardamit17@gmail.com>
2021-04-16 12:43:38 +0530

    Build Classes

--- DES-Modified.py
+++ DES-Modified.py
@@ -1,146 +1,160 @@
-PC_1 = [10, 6, 11, 7, 4, 12, 9, 3, 13, 16, 15, 14, 5, 8]
-PC_2 = [11, 10, 14, 3, 12, 5, 8, 4, 6, 7, 9, 13]
-IP = [13, 15, 6, 12, 10, 2, 5, 9, 11, 4, 3, 7, 1, 16, 14, 8]
-IP_dash = [13, 6, 11, 10, 7, 3, 12, 16, 8, 5, 9, 4, 1, 15, 2, 14]
-S_BOX = [[[2, 5, 4, 12, 0, 15, 9, 7, 6, 14, 1, 8, 11, 10, 13, 3], [2, 5, 4, 12, 0, 15, 9, 7, 6, 14, 1, 8, 11, 10, 13, 3], [2, 5, 4, 12, 0, 15, 9, 7, 6, 14, 1, 8, 11, 10, 13, 3], [2, 5, 4, 12, 0, 15, 9, 7, 6, 14, 1, 8, 11, 10, 13, 3]], [[6, 4, 14, 10, 13, 12, 3, 2, 5, 11, 0, 7, 1, 15, 9, 8], [6, 4, 14, 10, 13, 12, 3, 2, 5, 11, 0, 7, 1, 15, 9, 8], [6, 4, 14, 10, 13, 12, 3, 2, 5, 11, 0, 7, 1, 15, 9, 8], [6, 4, 14, 10, 13, 12, 3, 2, 5, 11, 0, 7, 1, 15, 9, 8]]]
-P = [3, 6, 4, 2, 1, 5, 7, 8]
-E = [8, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 1]
 
-#Matrix that determine the shift for each round of keys
-SHIFT = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
+gen = __import__('S-P Boxes Generator')
 
+class DES_M:
 
-def char_to_binary(x,sz):
-    return num_to_binary(ord(x),sz)
-
-def num_to_binary(x,sz):
-    res = bin(x)[2:]
-    while len(res) < sz :
-        res = '0' + res
-
-    return res
-
-def string_to_binary(s,sz):
-    res = ""
-    for i in s:
-        res += char_to_binary(i,sz)
-    arr = [int(i) for i in res]
-    return arr
-
-def bit_array_to_string(arr):
-    #split into group of 8
-    arr = split_array(arr,8)
-    fin = ""
-    for block in arr:
-        block = [str(x) for x in block]
-        block = ''.join(block)
-        fin += chr(int(block,2))
-    return fin
-
-def permutation(arr,box):
-    return [arr[i-1] for i in box]
-
-def expand(arr,box):
-    return [arr[i-1] for i in box]
-
-def split_array(arr,x):
-    return [arr[i:i+x] for i in range(0,len(arr),x)]
-
-def left_shift(arr,x):
-    return arr[x:] + arr[:x]
-
-def xor(arr1,arr2):
-    return [(arr1[i]^arr2[i]) for i in range(len(arr1))]
-
-def substitution(arr):
-    arr = split_array(arr,6)
-    fin = []
-    for i in range(len(arr)):
-        row = int(str(arr[i][0])+str(arr[i][5]),2)
-        col = int(''.join([str(i) for i in arr[i][1:5]]),2)
-
-        s_ij = S_BOX[i][row][col]
-        s_ij = num_to_binary(s_ij,4)
-
-        s_ij = [int(i) for i in s_ij]
-
-        fin.extend(s_ij)
-    return fin
-
-def generate_keys(key,no_of_keys,key_size):
-    keys = []
-    if(len(key) < key_size//8):
-        return []
-    key = key[:key_size//8]
-    key_arr = string_to_binary(key,8)
-    key = permutation(key_arr,PC_1)
-    tmp = split_array(key,(key_size-key_size//8)//2)
-    c0,d0 = tmp[0], tmp[1]
-    for i in range(no_of_keys):
-        c0,d0 = left_shift(c0,SHIFT[i]) , left_shift(d0,SHIFT[i])
-        keys.append(permutation(c0+d0,PC_2))
-    return keys
-
-def DEA(text,keys,no_of_rounds,block_size):
-    blocks = split_array(text,block_size//8)
-
-    cryp = []
-    for block in blocks:
-        block = string_to_binary(block,8)
-
-        #Apply Intial Permuatation
-        block = permutation(block,IP)
-
-        #Split the block in two halves
-        L,R = split_array(block,block_size//2)
-
-        for i in range(no_of_rounds):
-            #Expand the right half to 48 bits
-            R_dash = expand(R,E)
-
-            #Take Xor of block bits with key
-            R_dash = xor(keys[i],R_dash)
-
-            #S-box
-            R_dash = substitution(R_dash)
-
-            R_dash = permutation(R_dash,P)
-            
-
-            R_dash = xor(R_dash,L)
-
-            L = R
-            R = R_dash
-        tmp = R + L
+    def __init__(self,block_size,rounds,key,seed):
+        self.block_size = block_size
+        self.rounds = rounds
+        self.seed = seed
+        self.boxes = gen.ESP_BOX_Generator(block_size,seed)
+        self.keys = self.generate_keys(key,rounds,block_size)
         
-        tmp = permutation(tmp,IP_dash)
-        cryp.extend(tmp) 
-    cryp = bit_array_to_string(cryp)
-    return cryp
 
-def encrypt(msg,secret_key,rounds,block_size):
-    round_keys = generate_keys(secret_key,rounds,block_size)
-    return DEA(msg,round_keys,rounds,block_size)
+    def char_to_binary(self,x,sz):
+        return self.num_to_binary(ord(x),sz)
 
-def decrypt(c,secret_key,rounds,block_size):
-    round_keys = generate_keys(secret_key,rounds,block_size)
-    round_keys.reverse()
-    return DEA(c,round_keys,rounds,block_size)
+    def num_to_binary(self,x,sz):
+        res = bin(x)[2:]
+        while len(res) < sz :
+            res = '0' + res
 
-msg = "Hello wo"
-secret_key = "becret_k"
+        return res
+
+    def string_to_binary(self,s,sz):
+        res = ""
+        for i in s:
+            res += self.char_to_binary(i,sz)
+        arr = [int(i) for i in res]
+        return arr
+
+    def bit_array_to_string(self,arr):
+        #split into group of 8
+        arr = self.split_array(arr,8)
+        fin = ""
+        for block in arr:
+            block = [str(x) for x in block]
+            block = ''.join(block)
+            fin += chr(int(block,2))
+        return fin
+
+    def permutation(self,arr,box):
+        return [arr[i-1] for i in box]
+
+    def expand(self,arr,box):
+        return [arr[i-1] for i in box]
+
+    def split_array(self,arr,x):
+        return [arr[i:i+x] for i in range(0,len(arr),x)]
+
+    def left_shift(self,arr,x):
+        return arr[x:] + arr[:x]
+
+    def xor(self,arr1,arr2):
+        return [(arr1[i]^arr2[i]) for i in range(len(arr1))]
+
+    def substitution(self,arr):
+        arr = self.split_array(arr,6)
+        fin = []
+        for i in range(len(arr)):
+            row = int(str(arr[i][0])+str(arr[i][5]),2)
+            col = int(''.join([str(i) for i in arr[i][1:5]]),2)
+
+            s_ij = self.boxes.S_BOX[i][row][col]
+            s_ij = self.num_to_binary(s_ij,4)
+
+            s_ij = [int(i) for i in s_ij]
+
+            fin.extend(s_ij)
+        return fin
+
+    def generate_keys(self,key,no_of_keys,key_size):
+        keys = []
+        if(len(key) < key_size//8):
+            raise "Key length should be atleast "+str(key_size//8)
+        key = key[:key_size//8]
+        key_arr = self.string_to_binary(key,8)
+        key = self.permutation(key_arr,self.boxes.PC_1)
+        tmp = self.split_array(key,(key_size-key_size//8)//2)
+        c0,d0 = tmp[0], tmp[1]
+        for i in range(no_of_keys):
+            c0,d0 = self.left_shift(c0,self.boxes.SHIFT[i]) , self.left_shift(d0,self.boxes.SHIFT[i])
+            keys.append(self.permutation(c0+d0,self.boxes.PC_2))
+        return keys
+
+    def DEA(self,text,keys):
+        blocks = self.split_array(text,self.block_size//8)
+
+        cryp = []
+
+        #Stores the result after every round
+        cryp_inter = [[] for i in range(self.rounds)]
+
+        for block in blocks:
+            block = self.string_to_binary(block,8)
+
+            #Apply Intial Permuatation
+            block = self.permutation(block,self.boxes.IP)
+
+            #Split the block in two halves
+            L,R = self.split_array(block,self.block_size//2)
+
+            for i in range(self.rounds):
+                #Expand the right half to 48 bits
+                R_dash = self.expand(R,self.boxes.E)
+
+                #Take Xor of block bits with key
+                R_dash = self.xor(keys[i],R_dash)
+
+                #S-box
+                R_dash = self.substitution(R_dash)
+
+                R_dash = self.permutation(R_dash,self.boxes.P)
+                
+
+                R_dash = self.xor(R_dash,L)
+
+                L = R
+                R = R_dash
+
+                cryp_inter[i].extend(self.permutation(R+L,self.boxes.IP_dash))
+
+            tmp = R + L
+            
+            tmp = self.permutation(tmp,self.boxes.IP_dash)
+            cryp.extend(tmp) 
+            
+        cryp = self.bit_array_to_string(cryp)
+        return cryp , cryp_inter
+
+    def encrypt(self,msg):
+        return self.DEA(msg,self.keys)
+
+    def decrypt(self,c):
+        return self.DEA(c,list(reversed(self.keys)))
+
 
 
 #-------------Set Rounds and Block Size-----------------#
 
 rounds = 16
-block_size = 16
+block_size = 64
+seed = 20
+#-------------------------------------------------------#
+
+
+#-------------Set Plaintext and Secret key-----------------#
+
+msg = "Hello wo"
+secret_key = "aecret_k"
 
 #-------------------------------------------------------#
 
-cypher = encrypt(msg,secret_key,rounds,block_size)
+#des object
+des_o = DES_M(block_size,rounds,secret_key,seed)
+cypher , cypher_dash = des_o.encrypt(msg)
 
 print("Encrypt : %r" %cypher)
 
-m = decrypt(cypher,secret_key,rounds,block_size)
+m , _ = des_o.decrypt(cypher)
 print("Decrypt :",m)
